cb83503802c9e196b8b03c436e58efa0c6e184db
Matías Romero <matias.romero@lapieza.io>
2025-04-23 15:58:55 -0300

    Modified db conection

--- src/database.js
+++ src/database.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const { Pool } = require('pg');
+const { parse } = require('pg-connection-string');
+
+const config = parse(process.env.DATABASE_URL);
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: config.user,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.port,
   ssl: {
     rejectUnauthorized: false,
   },
